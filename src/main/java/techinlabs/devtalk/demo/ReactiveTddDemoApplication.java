a8f471c0b8a548c13c1de4aa0a8a77897357086d
Sukoreno Mukti <sukoreno.mukti@tiket.com>
2019-05-18 02:05:44 +0700

    init

---
+++ src/main/java/techinlabs/devtalk/demo/ReactiveTddDemoApplication.java
@@ -0,0 +1,13 @@
+package techinlabs.devtalk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTddDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveTddDemoApplication.class, args);
+	}
+
+}
